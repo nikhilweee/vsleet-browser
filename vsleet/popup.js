23247b83dd4b316228a96e8e40f271291ab95308
Nikhil Verma <nikhilweee@gmail.com>
2023-10-20 00:48:17 -0400

    Updated Image

--- vsleet/popup.js
+++ vsleet/popup.js
@@ -1,22 +1,29 @@
-const site = document.getElementById("site");
-const form = document.getElementById("form");
-
 async function initPopupWindow() {
+  const site = document.getElementById("site");
+  const input = document.getElementById("input");
+  const button = document.getElementById("button");
+
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   if (tab?.url) {
     let url = new URL(tab.url);
     const cookies = await chrome.cookies.getAll({ domain: url.hostname });
     site.textContent = `Cookies for ${url.hostname}`;
+    let cookieString = "";
     cookies.forEach((cookie) => {
-      const div = document.createElement("p");
-      div.innerHTML = `
-      <label for="input-${cookie.name}">${cookie.name}</label>
-      <input id="input-${cookie.name}" value="${cookie.value}"></input>`;
-      form.appendChild(div);
+      cookieString += `${cookie.name}=${cookie.value};`;
     });
-    console.log(cookies);
+    input.value = cookieString;
   }
+
+  button.addEventListener("click", () => {
+    const text = input.value;
+    navigator.clipboard.writeText(text);
+    button.value = "Copied!";
+    setTimeout(() => {
+      button.value = "Copy Cookies";
+    }, 2000);
+  });
 }
 
-initPopupWindow();
+document.addEventListener("DOMContentLoaded", initPopupWindow);
